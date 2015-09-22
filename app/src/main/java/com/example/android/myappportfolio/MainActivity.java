7fb5bed20bbc82d13b09ac6cc83606c9f7f63ac8
teevzees <jtiensivu@hotmail.com>
2015-09-22 14:28:49 -0400

    First Version
    I worked on it for 2 days maybe. I feel like I could add a lot to it.

---
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.myappportfolio;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** buttons call this method to launch other apps
+     * through the buttons style
+     *
+     * @param v
+     */
+    public void launchApp(View v){
+        Context c = getApplicationContext();
+        Button b = (Button) v;
+        CharSequence cs = b.getText();
+        int d = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(c, "This button will launch the " + cs, d);
+        toast.show();
+    }
+}
